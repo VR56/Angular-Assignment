2cd413358d1f3048abf50e1a5a98586a1d5eace4
VR56 <vishalraj31121998@gmail.com>
2020-04-07 15:59:55 +0530

    comitted

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -7,67 +7,8 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class UserServiceService{
-  // userData=[
-  //   {
-  //    "id":"RS",
-  //    "firstName": "Rakesh",
-  //    "lastName": "Sharma",
-  //    "age": 31,
-  //    "login": "RakeshSharma@xyz.com",
-  //    "password":"RS123",
-  //    "isDeleted": false 
-  //   },
-  //   {
-  //    "id":"RG",
-  //    "firstName": "Rahul",
-  //    "lastName": "Gupta",
-  //    "age": 35,
-  //    "login": "RahulGupta@xyz.com",
-  //    "password":"RG123",
-  //    "isDeleted": true 
-  //   },
-  //   {
-  //    "id":"RR",
-  //    "firstName": "Richik",
-  //    "lastName": "Roy",
-  //    "age": 41,
-  //    "login": "RichikRoy@xyz.com",
-  //    "password":"RR123",
-  //    "isDeleted": false 
-  //   },
-  //   {
-  //    "id":"AN",
-  //    "firstName": "Avishek",
-  //    "lastName": "Nair",
-  //    "age": 22,
-  //    "login": "AvishekNair@xyz.com",
-  //    "password":"AN123",
-  //    "isDeleted": false 
-  //   },
-  //   {
-  //    "id":"SC",
-  //    "firstName": "Sushant",
-  //    "lastName": "Choudhary",
-  //    "age": 29,
-  //    "login": "SushantChoudhary@xyz.com",
-  //    "password":"SC123",
-  //    "isDeleted": true 
-  //   },
-  //   {
-  //    "id":"RS",
-  //    "firstName": "Rohan",
-  //    "lastName": "Singh",
-  //    "age": 26,
-  //    "login": "RohanSingh@xyz.com",
-  //    "password":"RS1234",
-  //    "isDeleted": false 
-  //   }
-  //  ];
-
+  
   constructor(private http: HttpClient) { }
-  // getUserData(){
-  //   return this.userData;
-  // }
 
   getUsers() {
     return this.http.get<any[]>('http://localhost:8080/users');
